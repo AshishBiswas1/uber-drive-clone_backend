1b6554ba9c64666d9ba8c3c92437945837abdc30
Ashish Biswas <biswasashish655@gmail.com>
2025-09-05 19:24:15 +0530

    Function related trips.

--- Controller/tripController.js
+++ Controller/tripController.js
@@ -3,51 +3,148 @@ const catchAsync = require('./../util/catchAsync');
 const AppError = require('./../util/appError');
 
 exports.getAllTrips = catchAsync(async (req, res) => {
- const trips = await Trip.find();
+   const trips = await Trip.find();
 
- res.status(200).json({
-  status: 'success',
-  results: trips.length,
-  data: {
-   trips,
-  },
- });
+   res.status(200).json({
+      status: 'success',
+      results: trips.length,
+      data: {
+         trips,
+      },
+   });
 });
 
 exports.getTrip = catchAsync(async (req, res, next) => {
- const trip = await Trip.findById(req.params.id);
+   const trip = await Trip.findById(req.params.id);
 
- if (!trip) {
-  return next(new AppError('No trip found with that ID', 404));
- }
+   if (!trip) {
+      return next(new AppError('No trip found with that ID', 404));
+   }
 
- res.status(200).json({
-  status: 'success',
-  data: {
-   trip,
-  },
- });
+   res.status(200).json({
+      status: 'success',
+      data: {
+         trip,
+      },
+   });
 });
 
-exports.createTrip = catchAsync(async (req, res) => {
- const newTrip = await Trip.create({
-  riderId: req.body.riderId,
-  driverId: req.body.driverId,
-  pickupLocation: req.body.pickupLocation,
-  dropoffLocation: req.body.dropoffLocation,
-  status: req.body.status,
-  vehicleType: req.body.vehicleType,
-  fare: req.body.fare,
-  distance: req.body.distance,
-  duration: req.body.duration,
- });
+exports.createTrip = catchAsync(async (req, res, next) => {
+   const newTrip = await Trip.create({
+      riderId: req.body.riderId,
+      driverId: req.body.driverId,
+      pickupLocation: req.body.pickupLocation,
+      dropoffLocation: req.body.dropoffLocation,
+      status: req.body.status,
+      vehicleType: req.body.vehicleType,
+      fare: req.body.fare,
+      distance: req.body.distance,
+      duration: req.body.duration,
+   });
 
- res.status(201).json({
-  status: 'success',
-  data: {
-   trip: {
-    newTrip,
-   },
-  },
- });
+   res.status(201).json({
+      status: 'success',
+      data: {
+         trip: {
+            newTrip,
+         },
+      },
+   });
+});
+
+exports.updateTrip = catchAsync(async (req, res, next) => {
+   const trip = await Trip.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+   });
+
+   if (!trip) {
+      return next(new AppError('No trip found with that ID', 404));
+   }
+
+   res.status(200).json({
+      status: 'success',
+      data: {
+         trip,
+      },
+   });
+});
+
+exports.deleteTrip = catchAsync(async (req, res, next) => {
+   const trip = await Trip.findByIdAndDelete(req.params.id);
+
+   if (!trip) {
+      next(new AppError('No trip found with that ID', 404));
+   }
+
+   res.status(204).json({
+      status: 'success',
+      data: null,
+   });
+});
+
+exports.updateTripStatus = catchAsync(async (req, res, next) => {
+   const { status } = req.body;
+   const trip = await Trip.findByIdAndUpdate(
+      req.params.id,
+      {
+         status,
+         [`${status}At`]: new Date(),
+      },
+      { new: true }
+   );
+
+   res.status(200).json({
+      status: 'success',
+      data: {
+         trip,
+      },
+   });
+});
+
+exports.cancelTrip = catchAsync(async (req, res, next) => {
+   const { cancelledBy } = req.body;
+
+   const trip = await Trip.findByIdAndUpdate(
+      req.params.id,
+      {
+         status: `cancelled_By_${cancelledBy}`,
+         tripCompletedAt: new Date(),
+      },
+      {
+         new: true,
+      }
+   );
+
+   res.status(200).json({
+      status: 'success',
+      data: {
+         trip,
+      },
+   });
+});
+
+exports.getNearByTrips = catchAsync(async (req, res, next) => {
+   const { lng, lat, maxDistance } = req.query;
+
+   const trips = await Trip.find({
+      status: 'requested',
+      pickupLocation: {
+         $near: {
+            $geometry: {
+               type: 'Point',
+               coordinates: [lng * 1, lat * 1],
+            },
+            $maxDistance: maxDistance * 1000,
+         },
+      },
+   });
+
+   res.status(200).json({
+      status: 'success',
+      results: trips.length,
+      data: {
+         trips,
+      },
+   });
 });
--- Router/tripRouter.js
+++ Router/tripRouter.js
@@ -2,10 +2,21 @@ const express = require('express');
 const tripController = require('./../Controller/tripController');
 
 const router = express.Router();
+router.route('/nearby').get(tripController.getNearByTrips);
+
+router.route('/:id/status').patch(tripController.updateTripStatus);
+
+router.route('/:id/cancel').patch(tripController.cancelTrip);
 
 router
- .route('/')
- .get(tripController.getAllTrips)
- .post(tripController.createTrip);
+   .route('/')
+   .get(tripController.getAllTrips)
+   .post(tripController.createTrip);
+
+router
+   .route('/:id')
+   .get(tripController.getTrip)
+   .patch(tripController.updateTrip)
+   .delete(tripController.deleteTrip);
 
 module.exports = router;
